d9ad39157838154c5c1399bb73f56ea698028fb5
kiranbansode <kiran5120135@gmail.com>
2021-08-26 22:32:01 +0530

    add nestedErrorFinder function

---
+++ src/utils/helperFunctions/nestedErrorFinder.ts
@@ -0,0 +1,27 @@
+import { FieldErrors } from 'react-hook-form';
+
+const nestedErrorFinder = (fieldName: string, errorsObject: FieldErrors) => {
+	const names: string[] = fieldName.split('.');
+	let error;
+	switch (names.length) {
+		case 1:
+			error = errorsObject[names[0]]?.message;
+			break;
+
+		case 2:
+			error = errorsObject[names[1]]?.message;
+			break;
+
+		case 3:
+			error = errorsObject[names[2]]?.message;
+			break;
+
+		default:
+			error = null;
+			break;
+	}
+
+	return error;
+};
+
+export default nestedErrorFinder;
