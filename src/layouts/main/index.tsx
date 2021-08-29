cf9ee3289c09bee5f5811e8bc82eb19ea6f18c9e
kiranbansode <kiran5120135@gmail.com>
2021-08-29 14:13:36 +0530

    add Registration Page

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -2,12 +2,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import LoginPage from 'pages/LoginPage';
 import Dashboard from 'pages/Dashboard';
+import Registration from 'pages/Registration';
 
 const Main = () => (
 	<div>
 		<Switch>
 			<Route exact path="/" render={() => <LoginPage />} />
 			<Route exact path="/dashboard" render={() => <Dashboard />} />
+			<Route exact path="/register" render={() => <Registration />} />
 		</Switch>
 	</div>
 );
--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,1 +1,51 @@
-export {};
+import { useForm } from 'react-hook-form';
+import TextInputField from 'components/TextInputField';
+import PasswordInputField from 'components/PasswordInputField';
+
+const Registration = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit((data) => console.log(data))}>
+				<TextInputField
+					inputLabel="Firstname"
+					inputError={errors}
+					register={register('name.first')}
+				/>
+
+				<TextInputField
+					inputLabel="Middlename"
+					inputError={errors}
+					register={register('name.middle')}
+				/>
+
+				<TextInputField
+					inputLabel="Surname"
+					inputError={errors}
+					register={register('name.sur')}
+				/>
+
+				<PasswordInputField
+					inputLabel="Password"
+					inputError={errors}
+					register={register('password')}
+				/>
+
+				<PasswordInputField
+					inputLabel="Confirm Password"
+					inputError={errors}
+					register={register('confirmPassword')}
+				/>
+
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
+};
+
+export default Registration;
